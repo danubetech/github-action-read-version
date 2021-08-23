877df9dec8d2ea693a8ada06502404c41002412f
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2021-08-23 17:29:10 +0200

    chore: basic action setup

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:lts-alpine3.13
+
+LABEL "com.github.actions.name"="Read Version"
+LABEL "com.github.actions.description"="Github action to read configuration file and set version as env variable"
+LABEL "com.github.actions.icon"="mic"
+LABEL "com.github.actions.color"="blue"
+LABEL "version"="1.0.0"
+LABEL "repository"="https://github.com/danubetech/github-action-read-version"
+LABEL "maintainer"="Bernhard Fuchs <bernhard.fuchs@danubetech.com>"
+
+RUN apk update && apk upgrade && \
+    apk add --no-cache git
+
+WORKDIR /github-action-read-version/
+
+COPY app/package.json .
+RUN npm install
+
+COPY app/index.js .
+
+CMD node app/index.js
---
+++ app/index.js
@@ -0,0 +1,17 @@
+const examplePom = '<project xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://maven.apache.org/POM/4.0.0"\n' +
+    '\txsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">\n' +
+    '\n' +
+    '\t<modelVersion>4.0.0</modelVersion>\n' +
+    '\t<groupId>com.danubetech</groupId>\n' +
+    '\t<artifactId>uni-resolver-web</artifactId>\n' +
+    '\t<packaging>war</packaging>\n' +
+    '\t<name>uni-resolver-web</name>\n' +
+    '\n' +
+    '\t<parent>\n' +
+    '\t\t<groupId>decentralized-identity</groupId>\n' +
+    '\t\t<artifactId>uni-resolver</artifactId>\n' +
+    '\t\t<version>0.3-SNAPSHOT</version>\n' +
+    '\t</parent>\n' +
+    '</project>';
+
+const xml2js = require('xml2js');
