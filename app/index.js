1a6d54477ef65bb0840002e24fa063b9945c6605
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2021-08-23 18:29:31 +0200

    chore: add logs

--- app/index.js
+++ app/index.js
@@ -1,9 +1,14 @@
 const fs = require("fs")
 
 const pom = fs.readFileSync('/github/workspace/pom.xml')
+console.log('### Raw Pom', pom);
 
 const parseString = require('xml2js').parseString;
 
 parseString(pom, (err, result) => {
-    console.log(result.project.version[0])
+    if (err) {
+        throw err;
+    }
+    console.log('### Result', result);
+    console.log(result.project.version[0]);
 })
