67e0e01a4c8d42beefb630f0bf26c07ad30e058b
BernhardFuchs <bernhard.fuchs@danubetech.com>
2022-02-16 13:17:21 +0100

    feat: set releaseVersion and devVersion

--- app/index.js
+++ app/index.js
@@ -1,29 +1,50 @@
 const incrementMajorVersion = (parsedVersion) => {
     const splitString = parsedVersion.split('.');
     const majorVersion = parseInt(splitString[0]) + 1;
-    return `${majorVersion}.0.0`;
+
+    const releaseVersion = `${majorVersion}.0.0`;
+    const devVersion = `${majorVersion}.0-SNAPSHOT`;
+
+    return {releaseVersion, devVersion};
 }
 
 const incrementMinorVersion = (parsedVersion) => {
     const splitString = parsedVersion.split('.');
     const majorVersion = splitString[0];
-    const minorVersion = splitString[1].split('-')[0];
-    return `${majorVersion}.${minorVersion}.0`;
+    const minorVersion = parseInt(splitString[1].split('-')[0]);
+
+    let releaseVersion;
+    let devVersion;
+    if (splitString.length === 2) {
+        releaseVersion = `${majorVersion}.${minorVersion}.0`;
+        devVersion = `${majorVersion}.${minorVersion + 1}-SNAPSHOT`;
+    } else {
+        releaseVersion = `${majorVersion}.${minorVersion + 1}.0`;
+        devVersion = `${majorVersion}.${minorVersion + 2}-SNAPSHOT`;
+    }
+
+    return {releaseVersion, devVersion};
 }
 
 const incrementPatchVersion = (parsedVersion) => {
     const splitString = parsedVersion.split('.');
     const majorVersion = splitString[0];
-    const minorVersion = splitString[1].split('-')[0];
+    const minorVersion = parseInt(splitString[1].split('-')[0]);
 
+    let releaseVersion;
+    let devVersion;
     let patchVersion;
     if (splitString.length === 2) {
         patchVersion = 1;
+        releaseVersion = `${majorVersion}.${minorVersion - 1}.${patchVersion}`;
+        devVersion = `${majorVersion}.${minorVersion - 1}.${patchVersion}-SNAPSHOT`;
     } else {
-        patchVersion = splitString[2].split('-')[0];
+        patchVersion = parseInt(splitString[2].split('-')[0]);
+        releaseVersion = `${majorVersion}.${minorVersion}.${patchVersion + 1}`;
+        devVersion = `${majorVersion}.${minorVersion}.${patchVersion + 1}-SNAPSHOT`;
     }
 
-    return `${majorVersion}.${minorVersion}.${patchVersion}`;
+    return {releaseVersion, devVersion};
 }
 
 const readVersionMaven = (versionCore) => {
@@ -58,19 +79,21 @@ const core = require('@actions/core');
 const framework = core.getInput('framework', { trimWhitespace: true, required: true });
 const versionCore = core.getInput('version-core', { trimWhitespace: true, required: true });
 
-let version = ''
+let versions;
 switch (framework) {
     case 'maven':
-        version = readVersionMaven(versionCore);
+        versions = readVersionMaven(versionCore);
         break;
     case 'node':
     case 'nodejs':
         const packageJson = require('/github/workspace/package.json');
-        version = packageJson.version;
+        versions = packageJson.version;
         break;
     default:
-        throw Error(`Framework ${framework} not supported`)
+        throw Error(`Framework ${framework} not supported`);
 }
 
-console.log(`Setting version ${version} as output`);
-core.setOutput('version', version)
+console.log(`Setting releaseVersion ${versions.releaseVersion} as output`);
+core.setOutput('releaseVersion', versions.releaseVersion);
+console.log(`Setting devVersion ${versions.devVersion} as output`);
+core.setOutput('devVersion', versions.devVersion);
